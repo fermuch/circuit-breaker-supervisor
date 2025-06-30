d8fb887d177786d36211055ce53075eb22bc5daf
Sean Lang <slang800@gmail.com>
2025-06-29 23:43:14 -0400

    set version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CircuitBreakerSupervisor.MixProject do
   def project do
     [
       app: :circuit_breaker_supervisor,
-      version: "0.1.0",
+      version: "0.0.0-rc.1",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
