9dafe6eb67c500cacfb6c0aeb162fc85e5d17997
Sean Lang <slang800@gmail.com>
2025-07-02 22:23:14 -0400

    add another test

--- test/circuit_breaker_supervisor_test.exs
+++ test/circuit_breaker_supervisor_test.exs
@@ -20,6 +20,20 @@ defmodule CircuitBreakerSupervisorTest do
     assert %{active: 1} = Supervisor.count_children(DummySupervisor.Supervisor)
   end
 
+  test "starts child that was enabled after startup" do
+    # mark child disabled before startup
+    DummyFeatureFlagService.disable(:enable_me)
+    children = [sleepy_worker(id: :enable_me)]
+    start_supervised!({DummySupervisor, children: children})
+    assert %{active: 0} = Supervisor.count_children(DummySupervisor.Supervisor)
+
+    # enable child and verify that it is started
+    DummyFeatureFlagService.enable(:enable_me)
+    send(CircuitBreakerSupervisor.Monitor, :check_children)
+    Process.sleep(100)
+    assert %{active: 1} = Supervisor.count_children(DummySupervisor.Supervisor)
+  end
+
   test "terminates disabled children" do
     # startup with child enabled
     children = [sleepy_worker(id: :one), sleepy_worker(id: :disable_me)]
